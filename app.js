33fd8ee98d765b113b26c23527331fe1bc721ce1
achrafzebaa <zebaaachraf39@gmail.com>
2024-12-19 14:39:48 +0100

    Commit new version

--- app.js
+++ app.js
@@ -6,21 +6,24 @@ const mongoose=require("mongoose")
 const categorie = require("./models/categorie")
 const categorieRouter = require("./routes/categorie.route")
 const articleRouter =require("./routes/article.route")
+const cors=require('cors')
 dotenv.config()
 app.use(express.json())
-app.use("/api/categorie", categorieRouter)
-app.use('/api/scategorie', scategorieRouter);
-app.use('/api/article', articleRouter);
+app.use(cors())
+
 
 app.get("/acceuil",(req,res)=>{
 
 res.send("page acceuil")
 })
 // Connexion à la base données
-mongoose.connect(process.env.DATABASECLOUD)
+mongoose.connect(process.env.DATABASE)
 .then(() => {console.log("DataBase Successfully Connected");})
 .catch(err => { console.log("Unable to connect to database", err);
 process.exit(); });
+app.use("/api/categorie", categorieRouter)
+app.use('/api/scategorie', scategorieRouter);
+app.use('/api/article', articleRouter);
 
 
 
