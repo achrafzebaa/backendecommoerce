3e9ba7902cd9cec3187592b8e6f0ebadbe54ab63
achrafzebaa <zebaaachraf39@gmail.com>
2024-12-21 18:21:19 +0100

    Commit new version

--- app.js
+++ app.js
@@ -6,25 +6,27 @@ const mongoose=require("mongoose")
 const categorie = require("./models/categorie")
 const categorieRouter = require("./routes/categorie.route")
 const articleRouter =require("./routes/article.route")
-const cors=require('cors')
+const cors=require('cors');
+const path = require('path');
 dotenv.config()
 app.use(express.json())
 app.use(cors())
 
 
-app.get("/acceuil",(req,res)=>{
 
-res.send("page acceuil")
-})
+
 // Connexion à la base données
-mongoose.connect(process.env.DATABASE)
+mongoose.connect(process.env.DATABASECLOUD)
 .then(() => {console.log("DataBase Successfully Connected");})
 .catch(err => { console.log("Unable to connect to database", err);
 process.exit(); });
 app.use("/api/categorie", categorieRouter)
 app.use('/api/scategorie', scategorieRouter);
 app.use('/api/article', articleRouter);
-
+//dist reactjs
+app.use(express.static(path.join(__dirname, './client/build'))); // Route pour les pages non trouvées, redirige vers index.html
+app.get('*', (req, res) => { res.sendFile(path.join(__dirname,
+'./client/build/index.html')); });
 
 
 app.listen(process.env.PORT,()=>
